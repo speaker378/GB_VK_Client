734fc432fa2c21c044504edcac912cedc33dbfc4
speaker378 <speaker378debua@gmail.com>
2021-08-26 12:28:16 +0500

    homework for lesson 3

--- VKClient/VKClient/Controllers/FriendsTableViewController.swift
+++ VKClient/VKClient/Controllers/FriendsTableViewController.swift
@@ -8,82 +8,60 @@
 import UIKit
 
 class FriendsTableViewController: UITableViewController {
+    
+    private let friends = [
+        Friend(name: "Aragorn", avatar: UIImage(named: "Aragorn")),
+        Friend(name: "Boromir", avatar: UIImage(named: "Boromir")),
+        Friend(name: "Frodo", avatar: UIImage(named: "Frodo")),
+        Friend(name: "Galadriel", avatar: UIImage(named: "Galadriel")),
+        Friend(name: "Gandalf", avatar: UIImage(named: "Gandalf")),
+        Friend(name: "Legolas", avatar: UIImage(named: "Legolas")),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showFriendPhotos", sender: nil)
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return friends.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            let headerView = UIView()
+            headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 5
+        }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath)
+        cell.textLabel?.text = friends[indexPath.section].name
+        cell.imageView?.image = friends[indexPath.section].avatar
+        cell.textLabel?.textColor = UIColor.black
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 22)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        75
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let photosVC = segue.destination as? PhotosCollectionViewController else { return }
+        guard let userID = tableView.indexPathForSelectedRow?[0] else { return }
+        let photo = friends[userID].avatar
+        photosVC.userPhotos.append(UserPhoto(photo: photo))
     }
-    */
-
 }
---
+++ VKClient/VKClient/Models/Community.swift
@@ -0,0 +1,14 @@
+//
+//  Community.swift
+//  VKClient
+//
+//  Created by Сергей Черных on 25.08.2021.
+//
+
+import UIKit
+
+struct Community {
+    let name: String
+    let image: UIImage?
+    let members: UInt
+}
