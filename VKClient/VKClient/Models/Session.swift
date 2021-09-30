bba969e7212087480cf20dd104daede2701a6b38
speaker378 <speaker378debua@gmail.com>
2021-09-30 12:24:04 +0500

    Homework for lesson 1
    Client-server iOS applications

---
+++ VKClient/VKClient/Models/Session.swift
@@ -0,0 +1,15 @@
+//
+//  Session.swift
+//  VKClient
+//
+//  Created by Сергей Черных on 30.09.2021.
+//
+
+final class Session {
+    static let shared = Session()
+    
+    let token = ""
+    let userId = 0
+    
+    private init() {}
+}
