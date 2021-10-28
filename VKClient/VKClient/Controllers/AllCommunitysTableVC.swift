194a580c3d64338e9c7664fe46747380333a136d
speaker378 <speaker378debua@gmail.com>
2021-10-28 21:18:01 +0500

    Task 5

--- VKClient/VKClient/Controllers/AllCommunitysTableVC.swift
+++ VKClient/VKClient/Controllers/AllCommunitysTableVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class AllCommunitysTableVC: UITableViewController {
     
@@ -66,7 +67,39 @@ class AllCommunitysTableVC: UITableViewController {
 
 extension AllCommunitysTableVC: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        guard !searchText.isEmpty else { return }
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(AllCommunitysTableVC.reload), object: nil)
+            self.perform(#selector(AllCommunitysTableVC.reload), with: nil, afterDelay: 1.25)
+    }
+    
+    @objc func reload() {
+        guard let searchText = searchBar.text else { return }
+        guard searchText != "" else {
+            allCommunitys.removeAll()
+            tableView.reloadData()
+            return
+        }
         fetchCommunitys(text: searchText)
+        saveFindedCommunitysUserToFirebase(searchText)
+    }
+    
+    func saveFindedCommunitysUserToFirebase(_ text: String) {
+        let storageRef = Database.database().reference(withPath: "users")
+        let currentUser = storageRef.child(String(Session.shared.userId))
+        let history = currentUser.child("searchHistory")
+        history.child(getCurrentDateFormaterString()).setValue(text)
+    }
+    
+    func getCurrentDateFormaterString() -> String {
+        let now = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        let year = components.year ?? 0
+        let month = components.month ?? 0
+        let day = components.day ?? 0
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        let second = components.second ?? 0
+        let dataString = "\(year):\(month):\(day) - \(hour):\(minute):\(second)"
+        return dataString
     }
 }
